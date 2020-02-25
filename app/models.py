a31cf90f916e7a6d0250eef2af12ec0810522190
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-02-25 14:52:04 +0100

    Add required fields (not nullables)

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
 
     def __init__(self, username, email):
         self.username = username
