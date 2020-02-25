34effc17c88c75da03740327c0bb429ce2e8b486
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-02-25 16:29:47 +0100

    Implement CRUD User operations

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from config import Config
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
-from app import routes
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,8 @@
-from app import app
+from app import app, db
+from app.models import User
+from app.serializers import user_schema, users_schema
+from flask import abort, make_response, jsonify, request
+from sqlalchemy import or_
 
 
 @app.route('/')
@@ -8,24 +12,61 @@ def index():
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    return "Create user"
+    input_data = request.get_json()
+    errors = user_schema.validate(input_data)
+    if errors:
+        abort(make_response(jsonify(error=errors), 404))
+
+    if db.session.query(User).filter(or_(User.username == input_data.get('username'), User.email == input_data.get('email'))).first():
+        abort(make_response(jsonify(error="An user exists with this id or email"), 409))
+
+    new_user = user_schema.make_user(input_data)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(user_schema.dump(new_user))
 
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    return "Users list"
+    all_users = User.query.all()
+    return jsonify(users_schema.dump(all_users))
 
 
 @app.route('/users/<int:id>', methods=['GET'])
 def get_user_detail(id):
-    return "User detail"
+    user = User.query.get(id)
+    if not user:
+        abort(make_response(jsonify(error="User with id {} does not exists".format(id)), 404))
+
+    return jsonify(user_schema.dump(user))
 
 
 @app.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    return "Update user"
+    user = User.query.get(id)
+    if not user:
+        abort(make_response(jsonify(error="User with id {} does not exists".format(id)), 404))
+
+    input_data = request.get_json()
+    errors = user_schema.validate(input_data)
+    if errors:
+        abort(make_response(jsonify(error=errors), 404))
+
+    if db.session.query(User).filter(or_(User.username == input_data.get('username'), User.email == input_data.get('email'))).count() > 1:
+        abort(make_response(jsonify(error="An user exists with this id or email"), 409))
+
+    user.username = input_data.get('username')
+    user.email = input_data.get('email')
+    db.session.commit()
+    return jsonify(user_schema.dump(user))
 
 
 @app.route('/users/<int:id>', methods=['DELETE'])
 def delete_user(id):
-    return "Delete user"
+    user = User.query.get(id)
+    if not user:
+        abort(make_response(jsonify(error="User with id {} does not exists".format(id)), 404))
+
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user_schema.dump(user))
